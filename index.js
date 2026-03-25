53256bcd87883e97f10c4ff7d165580ab9b492af
palash2397 <palashpatil55@gmail.com>
2026-03-25 15:42:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import "dotenv/config.js"
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+
+const app = express();
+const port = process.env.PORT || 4006;
+
+// Middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("welcome to kickchain");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running on port ${port}`);
+});
