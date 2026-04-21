936605a06d9836e7414453d89d956485570323c9
palash2397 <palashpatil55@gmail.com>
2026-04-21 10:53:12 +0530

    Fix Firebase private key formatting for environment variables

--- utils/firbase/config.js
+++ utils/firbase/config.js
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 const firebaseConfig = JSON.parse(process.env.FIREBASE_JSON);
 firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, "\n");
 if (!admin.apps.length) {
-  // console.log("firebaseConfig", firebaseConfig);
+
   // console.log("firebaseConfig.private_key", firebaseConfig.private_key);
   admin.initializeApp({
     credential: admin.credential.cert(firebaseConfig),
